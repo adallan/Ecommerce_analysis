96bdc9b6b62e09afebff52e25426bd21f5a441cb
Alex <112234421+adallan@users.noreply.github.com>
2024-02-26 17:43:07 -0800

    Update sql_code.sql

--- sql_code.sql
+++ sql_code.sql
@@ -1,1 +1,10 @@
-
+#Creation of Customers Table
+  
+CREATE TABLE ecom_data.customers AS
+SELECT 
+    ROW_NUMBER() OVER () AS customer_id,
+    ship_postal_code as postal_code,
+    COUNT(DISTINCT order_id) AS num_orders,
+    ROUND(SUM(amount),2) AS total_spent
+FROM `ecom-dbt-project.ecom_data.Amazon_sales_report` 
+GROUP BY ship_postal_code
