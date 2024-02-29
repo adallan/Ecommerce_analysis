291349bdd9b70512e5ede5ce808eb2ac94741f36
Alex <112234421+adallan@users.noreply.github.com>
2024-02-29 15:12:20 -0800

    Update sql_code.sql
    Added code for cancelled orders and sales by inventory SKU

--- sql_code.sql
+++ sql_code.sql
@@ -1,4 +1,4 @@
-#Creation of Customers Table
+#Creation of Customers Table.
   
 CREATE TABLE ecom_data.customers AS
 SELECT 
@@ -8,3 +8,36 @@ SELECT
     ROUND(SUM(amount),2) AS total_spent
 FROM `ecom-dbt-project.ecom_data.Amazon_sales_report` 
 GROUP BY ship_postal_code
+
+#Cancelled Orders by customer_ids.
+SELECT 
+customer_id,
+Status,
+Category,
+COUNT(Order_ID) as num_cancelled,
+COALESCE(ROUND(SUM(Amount),2),0) as total_cancelled_amount
+#Changed nulls to 0 as the customer has not received a refund yet.
+FROM {{ref ('stg_customers')}} as customers
+JOIN {{ref ('stg_amazon_sales')}} as sales 
+ON customers.postal_code = sales.ship_postal_code
+WHERE status = 'Cancelled'
+GROUP BY customer_id ,Status, Category
+ORDER BY customer_id
+
+#Product sales by SKU codes/Inventory.
+SELECT 
+SKU_Code, 
+ROUND(SUM(Amount),2) as Item_sales, 
+inventory.Category, 
+COUNT(Order_ID) as Num_sold, 
+inventory.Size, 
+Color
+FROM {{ ref ('stg_inventory')}} as inventory
+JOIN {{ ref ('stg_amazon_sales')}} as sales 
+ON inventory.SKU_CODE = sales.SKU
+WHERE Amount != 0
+GROUP BY SKU_CODE, 
+inventory.Category, 
+inventory.Size, 
+Color
+ORDER BY num_sold DESC
