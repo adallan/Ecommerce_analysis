02a565826cdafcfa2a4009e2f60aa81292de7ca6
Alex <112234421+adallan@users.noreply.github.com>
2024-04-11 18:16:24 -0700

    Update sql_code.sql

--- sql_code.sql
+++ sql_code.sql
@@ -2,41 +2,44 @@
   
 CREATE TABLE ecom_data.customers AS
 SELECT 
-    ROW_NUMBER() OVER () AS customer_id,
-    ship_postal_code as postal_code,
-    COUNT(DISTINCT order_id) AS num_orders,
-    ROUND(SUM(amount),2) AS total_spent
+  ROW_NUMBER() OVER () AS customer_id,
+  ship_postal_code as postal_code,
+  COUNT(DISTINCT order_id) AS num_orders,
+  ROUND(SUM(amount),2) AS total_spent
 FROM `ecom-dbt-project.ecom_data.Amazon_sales_report` 
 GROUP BY ship_postal_code
 
 #Cancelled Orders by customer_ids.
 SELECT 
-customer_id,
-Status,
-COUNT(Order_ID) as num_cancelled,
-COALESCE(ROUND(SUM(Amount),2),0) as total_cancelled_amount
+  customer_id,
+  Status,
+  COUNT(Order_ID) as num_cancelled,
+  COALESCE(ROUND(SUM(Amount),2),0) as total_cancelled_amount
 #Changed nulls to 0 as the customer has not received a refund yet.
 FROM {{ref ('stg_customers')}} as customers
 JOIN {{ref ('stg_amazon_sales')}} as sales 
 ON customers.postal_code = sales.ship_postal_code
 WHERE status = 'Cancelled'
-GROUP BY customer_id ,Status
+GROUP BY
+  customer_id,
+  Status
 ORDER BY num_cancelled DESC
 
 #Product sales by SKU codes/Inventory.
 SELECT 
-SKU_Code, 
-ROUND(SUM(Amount),2) as Item_sales, 
-inventory.Category, 
-COUNT(Order_ID) as Num_sold, 
-inventory.Size, 
-Color
+  SKU_Code, 
+  ROUND(SUM(Amount),2) as Item_sales, 
+  inventory.Category, 
+  COUNT(Order_ID) as Num_sold, 
+  inventory.Size, 
+  Color
 FROM {{ ref ('stg_inventory')}} as inventory
 JOIN {{ ref ('stg_amazon_sales')}} as sales 
 ON inventory.SKU_CODE = sales.SKU
 WHERE Amount != 0
-GROUP BY SKU_CODE, 
-inventory.Category, 
-inventory.Size, 
-Color
+GROUP BY
+  SKU_CODE, 
+  inventory.Category, 
+  inventory.Size, 
+  Color
 ORDER BY num_sold DESC
