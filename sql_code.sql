246b7f8b199edea48cb5a60493b11d3dd6a3d761
Alex <112234421+adallan@users.noreply.github.com>
2024-04-23 17:10:06 -0700

    Update sql_code.sql

--- sql_code.sql
+++ sql_code.sql
@@ -43,3 +43,18 @@ GROUP BY
   inventory.Size, 
   Color
 ORDER BY num_sold DESC
+
+# High-value Customers by sales sums and averages.
+SELECT
+    customer_id,
+    COUNT(*) AS num_purchases,
+    SUM(Amount) AS total_spent,
+    ROUND(AVG(Amount),2) AS avg_order
+FROM {{ref ('stg_customers')}} AS customers
+JOIN {{ref ('stg_amazon_sales')}} AS sales
+ON customers.postal_code = sales.ship_postal_code
+WHERE 
+    Amount != 0 AND Status != 'Cancelled'
+GROUP BY customer_id
+HAVING total_spent > 10000
+ORDER BY total_spent DESC
